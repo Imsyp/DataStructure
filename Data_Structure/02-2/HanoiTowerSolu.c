f07bb8a066103021b3d8a16ccab12aa233a59229
Imsyp <tkddud386@gmail.com>
2023-08-09 02:17:20 +0900

    20230809

--- Data_Structure/02-2/HanoiTowerSolu.c
+++ Data_Structure/02-2/HanoiTowerSolu.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void HanoiTowerMove(int num, char from, char by, char to)
+void HanoiTowerMove(int num, char from, char by, char to)   //num개의 원반을 from에서 by를 거쳐서 to로 이동
 {
     if(num==1)
     {
-        printf("원반1을 %c에서 %c로 이동 \n", from, to);
+        printf("원반1을 %c에서 %c로 이동 \n", from, to);    //4단계
     }
     else
     {
-        HanoiTowerMove(num-1, from, to, by);
-        printf("원반%d을(를) %c에서 %c로 이동 \n", num, from, to);
-        HanoiTowerMove(num-1, by, from, to);
+        HanoiTowerMove(num-1, from, to, by);    //1단계
+        printf("원반%d을(를) %c에서 %c로 이동 \n", num, from, to);  //2단계
+        HanoiTowerMove(num-1, by, from, to);    //3단계
     }
 }
 
