272cae3d490bcecf7e63a0946fe0b1de442ddab0
Imsyp <tkddud386@gmail.com>
2023-08-13 20:28:11 +0900

    20230813

--- 05_2_Project_2/DBLinkedList.h
+++ 05_2_Project_2/DBLinkedList.h
@@ -1,5 +1,5 @@
-#ifndef __C_LINKED_LIST_H__
-#define __C_LINKED_LIST_H__
+#ifndef __DB_LINKED_LIST_H__
+#define __DB_LINKED_LIST_H__
 
 #define TRUE    1
 #define FALSE   0
@@ -10,27 +10,25 @@ typedef struct _node
 {
     Data data;
     struct _node * next;
+    struct _node * prev;
 } Node;
 
-typedef struct _CLL
+typedef struct _DLinkedList
 {
-    Node * tail;
+    Node * head;
     Node * cur;
-    Node * before;
     int numOfData;
-}   CList;
+} DBLinkedList;
 
-typedef CList List;
+typedef DBLinkedList List;
 
 void ListInit(List * plist);
-void LInsert(List * plist, Data data);  //꼬리에 노드 추가
-void LInsertFront(List * plist, Data data); //머리에 노드 추가
+void LInsert(List * plist, Data data); 
 
 int LFirst(List * plist, Data * pdata);
 int LNext(List * plist, Data * pdata);
+int LPrevious(List * plist, Data * pdata);
 
-Data LRemove(List * plist);
 int LCount(List * plist);
-
 
 #endif
