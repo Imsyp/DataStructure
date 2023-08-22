06e15c075a67c3b39c82f4935ffc39c3ca3dce3a
Imsyp <tkddud386@gmail.com>
2023-08-23 02:36:11 +0900

    20230823

--- 08_4_Project/ExpressionTree.c
+++ 08_4_Project/ExpressionTree.c
@@ -52,17 +52,7 @@ void ShowPrefixTypeExp(BTreeNode * bt)
 
 void ShowInfixTypeExp(BTreeNode * bt)
 {
-    if(bt==NULL)
-        return;
-    if(bt->left!=NULL || bt->right!=NULL)
-        printf("( ");
-
-    ShowInfixTypeExp(bt->left);
-    ShowNodeData(bt->data);
-    ShowInfixTypeExp(bt->right);
-
-    if(bt->left != NULL || bt->right != NULL)
-        printf(") ");
+    InorderTraverse(bt, ShowNodeData);    
 }
 
 void ShowPostfixTypeExp(BTreeNode * bt)
