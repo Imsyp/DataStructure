f2bb440f0239528f467c67c33b1c93cb0fbf4ab7
Imsyp <tkddud386@gmail.com>
2023-09-08 16:48:31 +0900

    20230908

---
+++ 10_2_Project/UsefulHeap.h
@@ -0,0 +1,25 @@
+#ifndef __USEFUL_HEAP_H__
+#define __USEFUL_HEAP_H__
+
+#define TRUE        1
+#define FALSE       0
+
+#define HEAP_LEN    100
+
+typedef int HData;  
+typedef int PriorityComp(HData d1, HData d2);   //책에는 typedef int (*PriorityComp)(HData d1, HData d2);로 돼있는데, 그렇게 하면 아래 선언할 때 이중포인터 선언이 돼버림 
+
+typedef struct _heap
+{
+    PriorityComp * comp;  
+    int numOfData;
+    HData heapArr[HEAP_LEN];
+} Heap;
+
+void HeapInit(Heap * ph, PriorityComp pc);
+int HIsEmpty(Heap * ph);
+
+void HInsert(Heap * ph, HData data);
+HData HDelete(Heap * ph);
+
+#endif
