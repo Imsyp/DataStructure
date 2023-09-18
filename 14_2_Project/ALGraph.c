5e7a1853f1250e2a976d1b9c39f99fb62975fe4d
Imsyp <tkddud386@gmail.com>
2023-09-18 17:10:23 +0900

    20230918

--- 14_2_Project/ALGraph.c
+++ 14_2_Project/ALGraph.c
@@ -1,5 +1,9 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "DLinkedList.h"
 #include "ALGraph.h"
+
+int WhoIsPrecede(int data1, int data2);
 
 void GraphInit(ALGraph * pg, int nv)
 {
@@ -9,10 +13,55 @@ void GraphInit(ALGraph * pg, int nv)
 
     pg->numV = nv;
     pg->numE = 0;
+
+    for(i=0; i<nv; i++)
+    {
+        ListInit(&(pg->adjList[i]));
+        SetSortRule(&(pg->adjList[i]), WhoIsPrecede);
+    }
 }
 
-void GraphDestroy(ALGraph * pg);
+void GraphDestroy(ALGraph * pg)
+{
+    if(pg->adjList != NULL)
+        free(pg->adjList);
+}
 
-void AddEdge(ALGraph * pg, int fromV, int toV);
+void AddEdge(ALGraph * pg, int fromV, int toV)
+{
+    LInsert(&(pg->adjList[fromV]), toV);
 
-void ShowGraphEdgeInfo(ALGraph * pg);
+    LInsert(&(pg->adjList[toV]), fromV);
+
+    pg->numE += 1;
+}
+
+
+
+void ShowGraphEdgeInfo(ALGraph * pg)
+{
+    int i;
+    int vx;
+
+    for(i=0; i<pg->numV; i++)
+    {
+        printf("%c와 연결된 정점: ", i+65);
+
+        if(LFirst(&(pg->adjList[i]), &vx))
+        {
+            printf("%c ", vx+65);
+
+            while(LNext(&(pg->adjList[i]), &vx))
+                printf("%c ", vx+65);
+                printf("\n");
+        }
+    }
+}
+
+int WhoIsPrecede(int data1, int data2)
+{
+    if(data1 < data2)
+        return 0;
+    else
+        return 1;
+}
