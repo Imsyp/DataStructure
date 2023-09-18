c06ec19488313e8070a434954017c0525b9503b4
Imsyp <tkddud386@gmail.com>
2023-09-18 15:54:28 +0900

    20230918

--- 13_2_Project/ChainedTableMain.c
+++ 13_2_Project/ChainedTableMain.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Person.h"
-#include "Table.h"
+#include "Table2.h"
 
 int MyHashFunc(int k)
 {
@@ -11,45 +11,44 @@ int MyHashFunc(int k)
 int main(void)
 {
     Table myTbl;
-
     Person * np;
     Person * sp;
     Person * rp;
 
     TBLInit(&myTbl, MyHashFunc);
 
-    np = MakePersonData(20120003, "Lee", "Seoul");
+    np = MakePersonData(900254, "LEE", "Seoul");
     TBLInsert(&myTbl, GetSSN(np), np);
-
-    np = MakePersonData(20130012, "KIM", "Jeju");
-    TBLInsert(&myTbl, GetSSN(np), np);
-
-    np = MakePersonData(20170049, "HAN", "Kangwon");
-    TBLInsert(&myTbl, GetSSN(np), np);
-
-
-    sp = TBLSearch(&myTbl, 20120003);
-    if(sp != NULL)
-        ShowPerInfo(sp);
     
-    sp = TBLSearch(&myTbl, 20130012);
+    np = MakePersonData(900139, "KIM", "Jeju");
+    TBLInsert(&myTbl, GetSSN(np), np);
+
+    np = MakePersonData(900827, "HAN", "Kangwon");
+    TBLInsert(&myTbl, GetSSN(np), np);
+
+
+    sp = TBLSearch(&myTbl, 900254);
     if(sp != NULL)
         ShowPerInfo(sp);
 
-    sp = TBLSearch(&myTbl, 20170049);
+    sp = TBLSearch(&myTbl, 900139);
+    if(sp != NULL)
+        ShowPerInfo(sp);
+
+    sp = TBLSearch(&myTbl, 900827);
     if(sp != NULL)
         ShowPerInfo(sp);
 
 
-    rp = TBLDelete(&myTbl, 20120003);
+    rp = TBLDelete(&myTbl, 900254);
     if(rp != NULL)
         free(rp);
 
-    rp = TBLDelete(&myTbl, 20130012);
+    rp = TBLDelete(&myTbl, 900139);
     if(rp != NULL)
         free(rp);
 
-    rp = TBLDelete(&myTbl, 20170049);
+    rp = TBLDelete(&myTbl, 900827);
     if(rp != NULL)
         free(rp);
 
--- 13_2_Project/DLinkedList.h
+++ 13_2_Project/DLinkedList.h
@@ -1,10 +1,12 @@
 #ifndef __D_LINKED_LIST_H__
 #define __D_LINKED_LIST_H__
 
+#include "Slot2.h"
+
 #define TRUE    1
 #define FALSE   0
 
-typedef int LData;
+typedef Slot LData;
 
 typedef struct _node
 {
