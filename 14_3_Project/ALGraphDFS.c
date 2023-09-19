033d1a8c496d3ea2482bfaf5e0a1020e5816b78d
Imsyp <tkddud386@gmail.com>
2023-09-19 16:17:09 +0900

    20230919

--- 14_3_Project/ALGraphDFS.c
+++ 14_3_Project/ALGraphDFS.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "DLinkedList.h"
 #include "ALGraphDFS.h"
+#include "ArrayBaseStack.h"
 
 int WhoIsPrecede(int data1, int data2);
 
@@ -31,9 +33,90 @@ void GrapgDestory(ALGraph * pg)
         free(pg->visitInfo);
 }
 
-void AddEdge(ALGraph * pg, int fromV, int toV);
+void AddEdge(ALGraph * pg, int fromV, int toV)
+{
+    LInsert(&(pg->adjList[fromV]), toV);
+    LInsert(&(pg->adjList[toV]), fromV);
 
-void ShowGraphEdgeInfo(ALGraph * pg);
+    pg->numE +=1;
+}
+
+void ShowGraphEdgeInfo(ALGraph * pg)
+{
+    int i;
+    int vx;
+
+    for(i=0; i<pg->numV; i++)
+    {
+        printf("%c와 연결된 정점: ", i + 65);
+
+        if(LFirst(&(pg->adjList[i]), vx));
+        {
+            printf("%c ", vx + 65);
+
+            while(LNext(&(pg->adjList[i]), vx))
+                printf("%c ", vx + 65);
+        }
+
+        printf("\n");
+    }
+}
+
+//visitV에 방문
+int VisitVertex(ALGraph * pg, int visitV)
+{
+    if (pg->visitInfo[visitV] == 0) //처음 방문하면 TRUE
+    {
+        pg->visitInfo[visitV] = 1;  //방문했으니 FALSE
+        printf("%c ", visitV + 65);
+        return TRUE;
+    }
+    return FALSE;
+}
 
 //정점의 정보 출력: DFS 기반
-void DFShowGraphVertex(ALGraph * pg, int startV);
+void DFShowGraphVertex(ALGraph * pg, int startV)
+{
+    Stack stack;
+    int visitV = startV;
+    int nextV;
+
+    StackInit(&stack);
+    VisitVertex(pg, visitV);
+    SPush(&stack, visitV);
+
+    while(LFirst(&(pg->adjList[visitV]), &nextV) == TRUE)   //첫 정점에 연결된 정점 방문
+    {
+        int visitFlag = FALSE;
+
+        if(VisitVertex(pg, nextV) == TRUE)
+        {
+            SPush(&stack, visitV);
+            visitV = nextV;
+            visitFlag = TRUE;
+        }
+        else
+        {
+            while(LNext(&(pg->adjList[visitV]), &nextV) == TRUE)    //첫 시도가 이미 방문한 상태인 경우 다른 정점 방문
+            {
+                if(VisitVertex(pg, nextV) == TRUE)
+                {
+                    SPush(&stack, visitV);
+                    visitV = nextV;
+                    visitFlag = TRUE;
+                    break;
+                }
+            }
+        }
+
+        if(visitFlag == FALSE)  //현재 정점에 연결된 모든 정점이 모두 방문한 상태인 경우
+        {
+            if(SIsEmpty(&stack) == TRUE)    //시작점으로 돌아온 경우
+                break;
+            else
+                visitV = SPop(&stack);      //왔던 길 되돌아가는 과정
+        }
+    }
+
+    memset(pg->visitInfo, 0, sizeof(int) * pg->numV);
+}
